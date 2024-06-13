aaa21553569603af54af0457f188aab701cbd89a
Misha <m.popov83@gmail.com>
2024-06-13 18:44:11 +0500

    2.1. Преобразование даты [#289476]

---
+++ src/main/java/ru/job4j/grabber/utils/HabrCareerDateTimeParser.java
@@ -0,0 +1,19 @@
+package ru.job4j.grabber.utils;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class HabrCareerDateTimeParser implements DateTimeParser {
+
+    @Override
+    public LocalDateTime parse(String parse) {
+        return LocalDateTime.parse(parse, DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+    }
+
+    public static void main(String[] args) {
+        DateTimeParser parser = new HabrCareerDateTimeParser();
+        LocalDateTime time = parser.parse("2024-06-07T13:41:13");
+        System.out.println("Year " + time.getYear());
+    }
+}
---
+++ src/test/java/ru/job4j/grabber/utils/HabrCareerDateTimeParserTest.java
@@ -0,0 +1,16 @@
+package ru.job4j.grabber.utils;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HabrCareerDateTimeParserTest {
+    @Test
+    void parse() {
+        DateTimeParser parser = new HabrCareerDateTimeParser();
+        LocalDateTime time = parser.parse("2024-06-07T13:41:13");
+        assertThat(time.getYear()).isEqualTo(2024);
+    }
+}
