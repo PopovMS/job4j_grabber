497f1a825f918c1545621f208d2b010764764363
Misha <m.popov83@gmail.com>
2024-06-16 14:48:29 +0500

    2.3. Загрузка деталей поста. [#285212]

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -16,7 +16,8 @@ public class HabrCareerParse {
     private static final int PAGECOUNT = 5;
 
     public static void main(String[] args) throws IOException {
-
+        HabrCareerParse desc = new HabrCareerParse();
+        desc.retrieveDescription("https://career.habr.com/vacancies/1000143941");
         for (int pageNumber = 1; pageNumber <= PAGECOUNT; pageNumber++) {
             String fullLink = "%s%s%d%s".formatted(SOURCE_LINK, PREFIX, pageNumber, SUFFIX);
             Connection connection = Jsoup.connect(fullLink);
@@ -29,7 +30,30 @@ public class HabrCareerParse {
                 Element dateElement = row.select(".vacancy-card__date").first();
                 String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
                 System.out.printf("%s %s %s %s%n", vacancyName, link, "дата размещения: ", dateElement.child(0).attr("datetime"));
+                System.out.println(desc.retrieveDescription(link));
             });
         }
+    }
+
+    private String retrieveDescription(String link)  {
+        Connection connection = Jsoup.connect(link);
+        Document document = null;
+        try {
+            document = connection.get();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Element row = document.select(".vacancy-description__text").first();
+        StringBuilder desc = new StringBuilder();
+        for (int r = 0; r < row.childNodeSize(); r++) {
+            String[] strings = row.child(r)
+                    .text()
+                    .split(";");
+            for (String string : strings) {
+                desc.append(string)
+                        .append("\n");
+            }
+        }
+        return desc.toString();
     }
 }
