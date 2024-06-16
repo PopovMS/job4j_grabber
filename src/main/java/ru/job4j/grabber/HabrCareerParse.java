fd4cd6a1d41e9fadab80835852946ef2dff71620
Misha <m.popov83@gmail.com>
2024-06-16 19:47:08 +0500

    2.4. HabrCareerParse [#285213]

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -5,34 +5,34 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import ru.job4j.grabber.utils.DateTimeParser;
+import ru.job4j.grabber.utils.HabrCareerDateTimeParser;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
-public class HabrCareerParse {
+public class HabrCareerParse implements Parse {
 
+    private final DateTimeParser dateTimeParser;
     private static final String SOURCE_LINK = "https://career.habr.com";
     public static final String PREFIX = "/vacancies?page=";
     public static final String SUFFIX = "&q=Java%20developer&type=all";
     private static final int PAGECOUNT = 5;
+    private static int tempIndex = 0;
 
-    public static void main(String[] args) throws IOException {
-        HabrCareerParse desc = new HabrCareerParse();
-        desc.retrieveDescription("https://career.habr.com/vacancies/1000143941");
+    public HabrCareerParse(DateTimeParser dateTimeParser) {
+        this.dateTimeParser = dateTimeParser;
+    }
+
+    public static void main(String[] args) {
+        HabrCareerParse init = new HabrCareerParse(new HabrCareerDateTimeParser());
         for (int pageNumber = 1; pageNumber <= PAGECOUNT; pageNumber++) {
             String fullLink = "%s%s%d%s".formatted(SOURCE_LINK, PREFIX, pageNumber, SUFFIX);
-            Connection connection = Jsoup.connect(fullLink);
-            Document document = connection.get();
-            Elements rows = document.select(".vacancy-card__inner");
-            rows.forEach(row -> {
-                Element titleElement = row.select(".vacancy-card__title").first();
-                Element linkElement = titleElement.child(0);
-                String vacancyName = titleElement.text();
-                Element dateElement = row.select(".vacancy-card__date").first();
-                String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
-                System.out.printf("%s %s %s %s%n", vacancyName, link, "дата размещения: ", dateElement.child(0).attr("datetime"));
-                System.out.println(desc.retrieveDescription(link));
-            });
+            System.out.println(init.list(fullLink).toString());
         }
+
     }
 
     private String retrieveDescription(String link)  {
@@ -55,5 +55,29 @@ public class HabrCareerParse {
             }
         }
         return desc.toString();
+    }
+
+    @Override
+    public List<Post> list(String link) {
+        List<Post> result = new ArrayList<>();
+            Connection connection = Jsoup.connect(link);
+            Document document = null;
+            try {
+                document = connection.get();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Elements rows = document.select(".vacancy-card__inner");
+            rows.forEach(row -> {
+                Element titleElement = row.select(".vacancy-card__title").first();
+                Element linkElement = titleElement.child(0);
+                String vacancyName = titleElement.text();
+                Element dateElement = row.select(".vacancy-card__date").first();
+                LocalDateTime time = dateTimeParser.parse(dateElement.child(0).attr("datetime"));
+                String url = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
+                System.out.println(this.retrieveDescription(url));
+                result.add(new Post(tempIndex++, vacancyName, url, this.retrieveDescription(url), time));
+            });
+        return result;
     }
 }
--- src/main/java/ru/job4j/grabber/utils/HabrCareerDateTimeParser.java
+++ src/main/java/ru/job4j/grabber/utils/HabrCareerDateTimeParser.java
@@ -8,7 +8,7 @@ public class HabrCareerDateTimeParser implements DateTimeParser {
 
     @Override
     public LocalDateTime parse(String parse) {
-        return LocalDateTime.parse(parse, DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+        return LocalDateTime.parse(parse, DateTimeFormatter.ISO_OFFSET_DATE_TIME);
     }
 
     public static void main(String[] args) {
